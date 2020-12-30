ed28109d818b55a6299c37851dc7f6a5bed23b0f
callmemiya <mari.zabrodina.01@mail.ru>
2020-12-30 09:17:08 +0300

    .

--- include/ClassPole.h
+++ include/ClassPole.h
@@ -13,10 +13,10 @@ public:
     std::vector<std::vector<int>> pole;
     int shots[10][10];
     int AdrKor[5][10];
-    extern Pole();
-    extern explicit Pole(std::vector<std::vector<int>> m);
-    extern void clear();
-    extern void print();
+    Pole();
+    explicit Pole(std::vector<std::vector<int>> m);
+    void clear();
+    void print();
 };
 
 //#include "ClassPole.cpp"
